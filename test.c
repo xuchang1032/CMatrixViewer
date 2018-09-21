b559565602e984cbfc1c75129ea4335a80831733
xuchang1032 <215586371@qq.com>
2018-09-21 23:37:26 +0100

    Let's rockin roll.
    Here is the mofification to the changes

--- test.c
+++ test.c
@@ -1,2 +1,19 @@
-sjdlkasjdlakjdlaksjd
-aksdjlaknvknvkad
+#include <iostream>
+#include "CGnssEpoch.h"
+
+int var[6] = {1, 2, 2, 5};
+
+int KaTest(double Inno[], int n)
+{
+	double sum = 0, thr = var[n];
+	for (int i = 0; i < n; i++)
+	{
+		sum += Inno[i]*Inno[i];
+	}
+	if (sum > thr)
+		return 1;
+	else
+		return 0;
+
+}
+
